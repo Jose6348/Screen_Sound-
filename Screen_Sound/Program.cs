fe971559a707dbeb39230d28e6bbfab94068ba82
SeuNome <jfalasco88@gmail.com>
2025-01-03 21:56:30 -0300

    Segunda aula de C#

--- Screen_Sound/Program.cs
+++ Screen_Sound/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Screen Sound
+
+string mensagemDeBoasVindas = "Seja bem vindo ao App";
+
+void ExibirMensagemDeBoasVindas()
+{
+Console.WriteLine("***************************");
+Console.WriteLine(mensagemDeBoasVindas);
+Console.WriteLine("***************************");
+}
+
+ExibirMensagemDeBoasVindas();
+
